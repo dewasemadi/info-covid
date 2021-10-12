ee74e2d3adbfd197046257326d00ee7d64bdd0f6
Nif21 <ilhamfaris@apps.ipb.ac.id>
2021-10-12 22:32:13 +0700

    Add Vaccines

--- pages/vaccine.js
+++ pages/vaccine.js
@@ -20,7 +20,7 @@ const contents = [
   },
   {
     title: "How Does A Vaccine Work?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet lobortis nisi, ac vestibulum metus efficitur vel. Sed venenatis auctor odio, quis pretium erat. In erat lorem, lacinia a lectus ultricies, congue porta tellus. Sed ut imperdiet metus. Mauris sagittis sodales ipsum, sed ultrices odio fringilla quis. Nullam a vehicula mauris. Maecenas in libero turpis.",
+    text: "Vaccines reduce risks of getting a disease by working with your body’s natural defenses to build protection. When you get a vaccine, your immune system responds.  It recognizes the invading germ, such as the virus or bacteria, produces antibodies, also remembers the disease and how to fight it.  The vaccine is therefore a safe and clever way to produce an immune response in the body, without causing illness.",
   },
   {
     title: "Facts & Myths About Vaccine",
