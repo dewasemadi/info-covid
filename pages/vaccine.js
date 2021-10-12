7c1160cc0ec27cbf94fbc9b887189c1452d0d08b
Nif21 <ilhamfaris@apps.ipb.ac.id>
2021-10-12 21:57:56 +0700

    Add Vaccine

--- pages/vaccine.js
+++ pages/vaccine.js
@@ -12,7 +12,11 @@ export default function Vaccine() {
         <title>Vaccine</title>
       </Head>
       <Container maxWidth='lg'>
-        <h2 className={classes.title}>Vaccine</h2>
+        <h2 className={classes.title}>What is Vaccination?</h2>
+        <h2 className={classes.title}>The Goals of Vaccination</h2>
+        <h2 className={classes.title}>Kinds of Vaccine</h2>
+        <h2 className={classes.title}>How Does a Vaccine Work?</h2>
+        <h2 className={classes.title}>Facts & Myths About Vaccine</h2>
       </Container>
     </div>
   );
