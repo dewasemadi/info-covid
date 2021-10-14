67caf182ef113f21be3be6ce525dc06a4e434f40
dewasemadi <dewasemadi3@gmail.com>
2021-10-14 09:58:14 +0800

    fix button navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -38,11 +38,6 @@ export default function Navigation() {
         value={value}
         onChange={(_, newValue) => {
           setValue(newValue);
-        }}
-        sx={{
-          "& .Mui-selected": {
-            fontSize: "12px",
-          },
         }}>
         <CssBaseline />
         {navLinks.map(({ title, path, icon }, idx) => (
@@ -56,6 +51,11 @@ export default function Navigation() {
             icon={icon}
             onClick={() => router.push(`${path}`)}
             key={idx}
+            sx={{
+              "& .Mui-selected": {
+                fontSize: "12px",
+              },
+            }}
           />
         ))}
       </BottomNavigation>
