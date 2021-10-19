df6780e9f9fca3dc55ae133911929b3a5729a97d
Nif21 <ilhamfaris@apps.ipb.ac.id>
2021-10-19 20:39:05 +0700

    edit faq

--- pages/faq.js
+++ pages/faq.js
@@ -13,7 +13,7 @@ export default function Faq() {
         <title>FAQ</title>
       </Head>
       <Container maxWidth='lg'>
-        <h2 className="title textGradient" style={{fontSize: "30px"}}>Frequently Asked Questions</h2>
+        <h2 style={{textAlign: "center"}}>Frequently Asked Questions</h2>
         <MyFaq />
       </Container>
     </div>
